b09483e1e136b8bdc232c93d92c5ef732ca61d6c
ChaoticRay <raymond.shen@newbury.edu>
2014-07-14 20:39:42 -0400

    Further Database Testing
    Further testing with the database connection

--- TestApp/TestApp/settings.py
+++ TestApp/TestApp/settings.py
@@ -58,8 +58,10 @@ WSGI_APPLICATION = 'TestApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DjangoTest',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
